f9cd62cc7f881d05db202587308820f16923d394
--global <--global>
2022-04-27 21:37:15 -0700

    hyy

--- mansi/src/App.js
+++ mansi/src/App.js
@@ -25,14 +25,82 @@ function App()
     // c=23;
     // console.log(c);
 
-      let arr=[10,20,30,40,50];
-      arr.map((a,i)=>console.log(a));
+      // let arr=[10,20,30,40,50];
+      // arr.map((a,i)=>console.log(a));
 
-      let intro=()=>{
-        console.log("mehta mansi");
-      }
-      intro();
-  
+      // let intro=()=>{
+      //   console.log("mehta mansi");
+      // }
+      // intro();  
+
+      // {
+      //     let arr = [10,"mansi",20,30,40,50];
+      //     console.log(arr[3]);
+      // }
+      // {
+      //     let obj={
+      //       id:10,
+      //       age:20,
+      //       per:30
+      //     }
+      //     console.log(obj.id);
+      // }
+
+
+      // push//
+  //  {
+
+  //     let arr = [10,"mansi",20,30,40,50];
+  //     arr.push(99);
+  //     console.log(arr);
+  //   }
+
+      //unshift//
+      // {
+      //   let arr = [10,"mansi",20,30,40,50];
+      //   arr.unshift(99);
+      //   console.log(arr);
+      // }
+
+      //pop//
+      // {
+      //   let arr = [10,"mansi",20,30,40,50];
+      //   arr.pop();
+      //   console.log(arr);
+      // }
+
+      //shift//
+      // {
+      //   let arr = [10,"mansi",20,30,40,50];
+      //   arr.shift();
+      //   console.log(arr);
+      // }
+      //spilice//
+      // {
+      //   let arr = [10,"mansi",20,30,40,50];
+      //   arr.splice(2,1);
+      //   console.log(arr);
+      // }
+           //tostring//
+    //  {
+    //   let arr = [10,"mansi",20,30,40,50];
+   
+    //   console.log(arr.toString());
+    // }
+          //some//
+    // {
+    //   let arr = [10,"mansi",20,30,40,50];
+    //    let res=arr.some((a)=>a>100);
+    //    console.log(res);
+    // }
+        //find//
+{
+                 let arr = [10,"mansi",20,30,40,50];
+                let res=arr.some((a)=>a>10);
+             console.log(res);
+}
+   
+
   return (
     <div className="App">
       <header className="App-header">
