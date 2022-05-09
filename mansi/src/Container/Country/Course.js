485caac7877d4616e844065a46862d0e13c8aeeb
--global <--global>
2022-05-08 22:27:56 -0700

    coursechange

---
+++ mansi/src/Container/Country/Course.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Course({Changec}) {
+    return (
+        <>
+      <div>
+                    
+                    {
+                         Changec ==='RNW1'?<p>Android</p>:<p>null</p>
+                    }
+        </div>
+
+        </>
+      
+    );
+}
+
+export default Course;
