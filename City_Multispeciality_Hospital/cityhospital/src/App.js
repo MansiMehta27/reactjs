b9c95e801f660d9066e296fb94148044fc7065ad
--global <--global>
2022-05-16 22:04:47 -0700

    cityhospital

---
+++ City_Multispeciality_Hospital/cityhospital/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./Componets/Header/Footer/Footer";
+import Header from "./Componets/Header/Header";
+import Home from "./Container/Home/Home";
+
+
+function App() {
+  return (
+    <>
+            <Header/>
+            <Home/>
+            <Footer/>
+    </>
+  );
+}
+
+export default App;
