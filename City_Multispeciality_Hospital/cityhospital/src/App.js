2cebceca6e62a733343de41346a859eacc7a346d
--global <--global>
2022-05-17 22:05:25 -0700

    helo

--- City_Multispeciality_Hospital/cityhospital/src/App.js
+++ City_Multispeciality_Hospital/cityhospital/src/App.js
@@ -1,3 +1,8 @@
+import { Route, Switch } from "react-router-dom";
+import About from "./Componets/About/About";
+import Contect from "./Componets/Contect/Contect";
+import Department from "./Componets/Departments/Department";
+import Doctors from "./Componets/Doctors/Doctors";
 import Footer from "./Componets/Header/Footer/Footer";
 import Header from "./Componets/Header/Header";
 import Home from "./Container/Home/Home";
@@ -7,8 +12,16 @@ function App() {
   return (
     <>
             <Header/>
-            <Home/>
+            <Switch>
+              <Route exact path={"/"} component={Home}/>
+              <Route exact path={"/departments"} component={Department}/>
+              <Route exact path={"/doctor"}component={Doctors}/>
+              <Route exact path={"/about"}component={About}/>
+              <Route exact path={"/contect"}component={Contect}/>
+              
+            </Switch>
             <Footer/>
+            
     </>
   );
 }
