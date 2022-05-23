957fd3cf0be628d5fd3be26eb8864b94bcce5123
--global <--global>
2022-05-23 10:44:45 +0530

    fgfth

--- mansi/famms-1.0.0/jsxproject/src/App.js
+++ mansi/famms-1.0.0/jsxproject/src/App.js
@@ -1,7 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import Footer from "./Componets/Footer/Footer";
+import Header from "./Componets/Header/Header";
+import Blog from "./Containers/Blogs/Blog";
+import Contect from "./Containers/Contect/Contect";
+import Home from "./Containers/Home/Home";
+import Product from "./Containers/Products/Product";
+
+
 function App() {
   return (
       <>
-              
+            <Header/>
+
+                      <Switch>
+                                  <Route exact path={"/"}component={Home}/>
+                                  <Route exact path={"/products"}component={Product}/>
+                                  <Route exact path={"/Blogs"}component={Blog}/>
+                                  <Route exact path={"/contect"}component={Contect}/>
+                                
+                      </Switch>
+
+              <Footer/>  
       </>
   );
 }
