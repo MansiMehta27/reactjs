b36e2fa5376fdab0562b409080c0184229c6b4a0
MansiMehta27 <100567987+MansiMehta27@users.noreply.github.com>
2022-07-11 08:50:52 +0530

    Update medicin.action.js

--- City_Multispeciality_Hospital/admin/src/Redux/Action/medicin.action.js
+++ City_Multispeciality_Hospital/admin/src/Redux/Action/medicin.action.js
@@ -1,28 +1,71 @@
 import*as ActionTypes from "../ActionTypes";
-
 import { base_url } from "../../Sharad/baseurl";
-
-
 export const getmedicines=()=>(dispatch)=>{
     try{
-        fetch(base_url + 'medisin')
-        .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-        var errmess = new Error(error.message);
-        throw errmess;
-      })
-      .then(response => response.json())
-      .then(medicines => dispatch({ type: ActionTypes.GET_MEDISION, payload: medicines }))
-      .catch(error=>console.log(error));
+     dispatch(loadingMedicine());
+     setTimeout(function () {
+      return fetch (base_url + 'medisin')
+      .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      var errmess = new Error(error.message);
+      throw errmess;
+    })
+    .then(response => response.json())
+    .then(medisine => dispatch(({ type: ActionTypes.GET_MEDISION, payload: medisine })))
+    .catch((error)=>dispatch(errorMedicine(error.message)))
+    },2000)
     }catch(error){
-            console.log(Error);
+           dispatch(errorMedicine(error.message))
     }
 }
+
+export const loadingMedicine =()=>(dispatch)=>{
+  dispatch({type:ActionTypes.LOADING_MEDICINES});
+}
+export const errorMedicine =(error)=>(dispatch)=>{
+  dispatch({type:ActionTypes.ERROR_MEDICINES,payload:error});
+}
+export const addMedicines = (data)=>(dispatch)=>{
+  try{
+    dispatch(loadingMedicine());
+    setTimeout(function(){
+     return fetch (base_url + 'medisin',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+     .then(response => {
+     if (response.ok) {
+       return response;
+     } else {
+       var error = new Error('Error' + response.status + ': ' + response.statusText);
+       error.response = response;
+       throw error;
+     }
+   },
+   error => {
+     var errmess = new Error(error.message);
+     throw errmess;
+   })
+   .then(response => response.json())
+   .then(medisine => dispatch({ type: ActionTypes.POST_MEDICINES, payload: medisine }))
+   .catch(error=>dispatch(errorMedicine(error.message)))
+   
+   },2000 )
+     
+   }catch(error){
+          dispatch(errorMedicine(error.message));
+   }
+}
+
+
